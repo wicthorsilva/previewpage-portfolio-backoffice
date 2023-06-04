137b0c039bf2b7a527ef882dfb31c25bb1cacdc3
wicthorsilva <luizwicthor@gmail.com>
2023-06-04 17:38:42 -0300

    UPDATE API

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: apiUrl,
+  baseURL: "https://sitepessoal-api.onrender.com/api",
 });
 
 export default api;
