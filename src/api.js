71b8db4ebaf94017af1bd0d19f5aa485084ac4d8
wicthorsilva <luizwicthor@gmail.com>
2023-06-07 12:07:26 -0300

    Ajuste de variavel de ambiente com a atualização do vite ^4.3.4

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: "https://sitepessoal-api.onrender.com/api",
+  baseURL: apiUrl,
 });
 
 export default api;
